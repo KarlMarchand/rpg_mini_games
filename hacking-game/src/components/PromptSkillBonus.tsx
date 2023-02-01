03e9de4196aa33a15c15df47539bd1e368b174e9
Karlipouette <karl-marchand@hotmail.com>
2023-02-01 14:52:02 -0500

    Prompt component

--- hacking-game/src/components/PromptSkillBonus.tsx
+++ hacking-game/src/components/PromptSkillBonus.tsx
@@ -1,5 +1,26 @@
-const PromptSkillBonus: React.FC<{ setSkill: (bonus: number) => void }> = ({ setSkill }) => {
-	return <div>PromptSkillBonus</div>;
+import TerminalInput from "./TerminalInput";
+
+type Props = {
+	setSkill: (bonus: number) => void;
+};
+
+const PromptSkillBonus: React.FC<Props> = ({ setSkill }) => {
+	// Make sure the string is a number else put 0
+	const handleSubmit = (value: string) => {
+		let number = Number(value);
+		if (Number.isNaN(number)) {
+			number = 0;
+		}
+		setSkill(number);
+	};
+
+	return (
+		<div className="skill-prompt">
+			Welcome back Sarlacc User! We're ready for this new challenge but will first need to know what your skill
+			bonus is with this kind of tool.
+			<TerminalInput label={"Enter your skill bonus:"} onSubmit={handleSubmit} numbersOnly={true} maxLength={4} />
+		</div>
+	);
 };
 
 export default PromptSkillBonus;
