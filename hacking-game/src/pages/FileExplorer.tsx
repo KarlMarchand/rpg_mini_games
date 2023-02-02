522a70a51813262bbb8f728c936197d1da48bd89
Karlipouette <karl-marchand@hotmail.com>
2023-02-02 12:57:29 -0500

    Computer Home Screen component

--- hacking-game/src/pages/FileExplorer.tsx
+++ hacking-game/src/pages/FileExplorer.tsx
@@ -1,12 +1,29 @@
-const FileExplorer: React.FC<{ files: File[] }> = ({ files }) => {
+import { useState } from "react";
+import { FilesDetails } from "@/types/hacking";
+import ComputerHomeScreen from "@/components/ComputerHomeScreen";
+
+const FileExplorer: React.FC<{ files: FilesDetails[] }> = ({ files }) => {
+	const [showHomeScreen, setShowHomeScreen] = useState<boolean>(true);
+
+	const changeScreen = () => {
+		setTimeout(() => {
+			setShowHomeScreen(false);
+		}, 5000);
+	};
+
 	return (
 		<>
-			<h1>FileExplorer</h1>
-			<div className="files-explorer">
-				{files.map((file) => (
-					<p key={file.name}>{file.name}</p>
-				))}
-			</div>
+			{showHomeScreen && <ComputerHomeScreen onFinish={changeScreen} />}
+			{!showHomeScreen && (
+				<>
+					<h1>Files Explorer</h1>
+					<div className="files-explorer">
+						{files.map((file) => (
+							<p key={file.name}>{file.name}</p>
+						))}
+					</div>
+				</>
+			)}
 		</>
 	);
 };
